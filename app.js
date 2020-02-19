ba03892fbb2d9f72ace2b0012734b9a6f080e533
Scott Campbell <stc9486@gmail.com>
2020-02-19 11:45:29 -0800

    group-3

--- app.js
+++ app.js
@@ -1,77 +1,55 @@
 const express = require('express');
 const app = express();
-const request = require('superagent');
-const darkSkyData = require('./data/darksky.json');
-const geoData = require('./data/geo.json');
+//const request = require('superagent');
+const geoData = require('./data/geo.js');
 const weather = require('./data/darksky.js');
+const cors = require('cors');
 
-// const geoParse = geoData[0];
-// console.log(geoData[0].display_name);
+app.use(cors());
+
+//initialize state variables
+let lng;
+let lat;
+
 app.get('/location/', (req, res) => {
+    const location = request.query.search;
     const cityData = geoData.results[0];
-    console.log(req.query);
+    lat = cityData.geometry.location.lat,
+    lng = cityData.geometry.location.lng,
     res.json({
+        
         name: req.query.name,
         formatted_query: cityData.formatted_address,
-        lat: cityData.geometry.location.lat,
-        lon: cityData.geometry.location.lon
+        //in 23 and 24 we set state
+        latitude: cityData.geometry.location.lat,
+        longitude: cityData.geometry.longitude.lng
     });
-});
-
-//?search=Portland
-// app.get('/location/search-query/', (req, res) => {
-//     res.json({
-//         search_query: 'whatever',
-//         formatted_query: 'other-thing',
-//         location_name: geoParse.display_name,
-//         lat: geoParse.lat,
-//         lon: geoParse.lon
-//     });
-// });
-
-//user will pass a search query param called 'portland' you need to match that to lat and lon
-
-// console.log(darkSkyData.currently.summary);
-
-app.get('/weather/:lat/:lon/', (req, res) => {
-    res.json([
-        {
-            lat: darkSkyData.latitude,
-            lon: darkSkyData.longitude,
-            forecast: darkSkyData.currently.summary,
-            time: dateString
-        }
-    ]);
 });
 
 //eventually this will be replaced by an API call
     // will need lat/lon though
-let getWeatherData = (lat, lon) => {
+let getWeatherData = (lat, lng) => {
   return weather.daily.data.map(forecast => {
-  return 
-        {
+      return {
           forecast: forecast.summary,
-          time: new Date(forecast.time);
-    }
-  })
-}
+          time: new Date(forecast.time * 1000);
+    };
+  });
+};
+
 
 app.get('/weather', (req, res) => {
-    const portlandWeather = getWeatherData(/*lat*/*lon);
+    const portlandWeather = getWeatherData(lat, lng);
   
 })
 
-// let theDate = new Date(darkSkyData.currently.time * 1000);
-// let dateString = theDate.toUTCString();
-// // console.log(dateString);
+app.listen(3000, () => { console.log('running . . .')});
 
 app.get('*', (req, res) => {
-    res.send({
-        uhOh: '404'
-    });
+  res.send({
+      uhOh: '404'
+  });
 });
-
-app.listen(3000, () => { console.log('running . . .')});
 
 module.exports = {
     app: app,
