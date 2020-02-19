4b51ae0f167574a71bc95b3731f8f8bb4b86c233
Scott Campbell <stc9486@gmail.com>
2020-02-19 00:21:57 -0800

    setup...

--- app.js
+++ app.js
@@ -1,18 +1,90 @@
+//App Dependencies:
 const express = require('express');
-const app = express();
 
-app.get('/whatever/:latitude/:longitude', (req, res) => {
-    res.json {(
-      some:'json'
-    )};
+//"require" parses JSON into array/object
+const geoData = require('./data/geo.json');
+const darkSkyData = require('./data/darksky.json');
+
+//App Setup:
+
+// create Express App
+const app = express();
+// set the port where the server will run
+const port = 3000;
+//enable middleware
+
+//Set API Routes:
+  //route skeleton/abstract
+    // app.<http method>(<path>, (handler, handler));
+
+
+app.get('/location', (req, res) => {
+    try {
+        const location = req.query.location;
+        const result = getLatLng(location);
+        res.status(200).json(result);     
+    }
+    catch (err) {
+        res.status(500).send('Failed attempt. Please try again.');
+    }
 });
 
+//Helper Functions
+let getLatLng = (location) => {
+    if(location === 'bad location') {
+        throw new Error();
+    }
+    //ignore location for now, return hard-coded file?
+    //api call goes here:
+
+    //convert to desired data format:
+    return toLocation(geoData);
+};
+
+let toLocation = (dataRoute) => {
+    const firstResult = dataRoute.results[0];
+    const geomety = firstResult.geometry;
+};
+
+// THIS IS DARKSKY
+
+app.get('/weather', (req, res) => {
+  try {
+      const weather = req.query.weather;
+      const result = getForecast(weather);
+      res.status(200).json(result);     
+  }
+  catch (err) {
+      res.status(500).send('Failed attempt. Please try again.');
+  }
+});
+
+let getForecast = (weather) => {
+  if(weather === 'bad weather') {
+    throw new Error();
+}
+//ignore location for now, return hard-coded file?
+//api call goes here:
+
+//convert to desired data format:
+return toLocation(darkSkyData);
+};
+}
+
+let toAbc = (route) => {
+  for darkSkyData;
+}
+
+//this listens in on ?
 app.listen(3000, () => {
     console.log('idk');
 });
 
-app.get(*, (req, res) => {
-  res.json({
-    nada: '404'
-  });
-})
+//this is some universal response
+app.get('*', (req, res) => res.send('404'));
+
+module.exports = {
+    app,
+    toLocation,
+    getLatLng
+};
