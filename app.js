7e53fec6331a34deed5cf1c0c7d8ebb0252548e9
Scott Campbell <stc9486@gmail.com>
2020-02-19 10:58:38 -0800

    2nd-group

--- app.js
+++ app.js
@@ -1,90 +1,78 @@
-//App Dependencies:
 const express = require('express');
-
-//"require" parses JSON into array/object
-const geoData = require('./data/geo.json');
-const darkSkyData = require('./data/darksky.json');
-
-//App Setup:
-
-// create Express App
 const app = express();
-// set the port where the server will run
-const port = 3000;
-//enable middleware
+const request = require('superagent');
+const darkSkyData = require('./data/darksky.json');
+const geoData = require('./data/geo.json');
+const weather = require('./data/darksky.js');
 
-//Set API Routes:
-  //route skeleton/abstract
-    // app.<http method>(<path>, (handler, handler));
-
-
-app.get('/location', (req, res) => {
-    try {
-        const location = req.query.location;
-        const result = getLatLng(location);
-        res.status(200).json(result);     
-    }
-    catch (err) {
-        res.status(500).send('Failed attempt. Please try again.');
-    }
+// const geoParse = geoData[0];
+// console.log(geoData[0].display_name);
+app.get('/location/', (req, res) => {
+    const cityData = geoData.results[0];
+    console.log(req.query);
+    res.json({
+        name: req.query.name,
+        formatted_query: cityData.formatted_address,
+        lat: cityData.geometry.location.lat,
+        lon: cityData.geometry.location.lon
+    });
 });
 
-//Helper Functions
-let getLatLng = (location) => {
-    if(location === 'bad location') {
-        throw new Error();
+//?search=Portland
+// app.get('/location/search-query/', (req, res) => {
+//     res.json({
+//         search_query: 'whatever',
+//         formatted_query: 'other-thing',
+//         location_name: geoParse.display_name,
+//         lat: geoParse.lat,
+//         lon: geoParse.lon
+//     });
+// });
+
+//user will pass a search query param called 'portland' you need to match that to lat and lon
+
+// console.log(darkSkyData.currently.summary);
+
+app.get('/weather/:lat/:lon/', (req, res) => {
+    res.json([
+        {
+            lat: darkSkyData.latitude,
+            lon: darkSkyData.longitude,
+            forecast: darkSkyData.currently.summary,
+            time: dateString
+        }
+    ]);
+});
+
+//eventually this will be replaced by an API call
+    // will need lat/lon though
+let getWeatherData = (lat, lon) => {
+  return weather.daily.data.map(forecast => {
+  return 
+        {
+          forecast: forecast.summary,
+          time: new Date(forecast.time);
     }
-    //ignore location for now, return hard-coded file?
-    //api call goes here:
-
-    //convert to desired data format:
-    return toLocation(geoData);
-};
-
-let toLocation = (dataRoute) => {
-    const firstResult = dataRoute.results[0];
-    const geomety = firstResult.geometry;
-};
-
-// THIS IS DARKSKY
+  })
+}
 
 app.get('/weather', (req, res) => {
-  try {
-      const weather = req.query.weather;
-      const result = getForecast(weather);
-      res.status(200).json(result);     
-  }
-  catch (err) {
-      res.status(500).send('Failed attempt. Please try again.');
-  }
+    const portlandWeather = getWeatherData(/*lat*/*lon);
+  
+})
+
+// let theDate = new Date(darkSkyData.currently.time * 1000);
+// let dateString = theDate.toUTCString();
+// // console.log(dateString);
+
+app.get('*', (req, res) => {
+    res.send({
+        uhOh: '404'
+    });
 });
 
-let getForecast = (weather) => {
-  if(weather === 'bad weather') {
-    throw new Error();
-}
-//ignore location for now, return hard-coded file?
-//api call goes here:
-
-//convert to desired data format:
-return toLocation(darkSkyData);
-};
-}
-
-let toAbc = (route) => {
-  for darkSkyData;
-}
-
-//this listens in on ?
-app.listen(3000, () => {
-    console.log('idk');
-});
-
-//this is some universal response
-app.get('*', (req, res) => res.send('404'));
+app.listen(3000, () => { console.log('running . . .')});
 
 module.exports = {
-    app,
-    toLocation,
-    getLatLng
+    app: app,
 };
